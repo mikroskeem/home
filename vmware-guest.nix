e4f802e780e67691e104df1c678aaa4c218e45fd
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-06-09 14:22:49 +0300

    VMWare guest tools build fine now

--- vmware-guest.nix
+++ vmware-guest.nix
@@ -1,19 +1,6 @@
-{ config, lib, ... }:
+{ ... }:
 
 {
-  # TODO(2021-05-13) - open-vm-tools does not build
-  nixpkgs.overlays = [
-    (self: super:
-      let
-        olderPkgs = import (builtins.fetchTarball https://github.com/r-ryantm/nixpkgs/archive/63d650556b6917b3a4075f81bd7cdff3d8f3bb40.tar.gz) { };
-      in
-      {
-        open-vm-tools = olderPkgs.open-vm-tools;
-      })
-  ];
-
   virtualisation.vmware.guest.enable = true;
-  environment.variables = lib.optionalAttrs (config.virtualisation.vmware.guest.enable) {
-    "WLR_NO_HARDWARE_CURSORS" = "1";
-  };
+  environment.variables."WLR_NO_HARDWARE_CURSORS" = "1";
 }
