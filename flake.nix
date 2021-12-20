72a820a14c4f8bdfce86879f5949c4162b2d7142
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-12-20 16:07:03 +0200

    Add flake-utils, set up dev shell

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     darwin.url = "github:lnl7/nix-darwin/master";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager.url = "github:nix-community/home-manager/master";
     impermanence.url = "github:nix-community/impermanence/master";
     nixos-generators.url = "github:nix-community/nixos-generators";
@@ -21,58 +22,79 @@
     sops.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager, impermanence, impure-local, ... }@inputs: {
-    nixosModules.nixpkgsCommon = { ... }: {
-      nix.nixPath = [ "nixpkgs=${nixpkgs.outPath}" ];
-      nix.registry.nixpkgs.flake = nixpkgs;
-      home-manager.useGlobalPkgs = true;
+  outputs = { self, nixpkgs, darwin, flake-utils, home-manager, impermanence, impure-local, ... }@inputs:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    {
+      nixosModules.nixpkgsCommon = { ... }: {
+        nix.nixPath = [ "nixpkgs=${nixpkgs.outPath}" ];
+        nix.registry.nixpkgs.flake = nixpkgs;
+        home-manager.useGlobalPkgs = true;
 
-      nixpkgs.config = {
-        allowUnfree = true;
+        nixpkgs.config = {
+          allowUnfree = true;
+        };
       };
-    };
 
-    darwinModules.nixpkgsCommon = self.nixosModules.nixpkgsCommon;
+      darwinModules.nixpkgsCommon = self.nixosModules.nixpkgsCommon;
 
-    nixosModules.impermanenceConfig = import ./modules/impermanence.nix;
+      nixosModules.impermanenceConfig = import ./modules/impermanence.nix;
 
-    darwinConfigurations."miniskeem" = darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules = [
-        self.darwinModules.nixpkgsCommon
-        home-manager.darwinModules.home-manager
-        ./systems/miniskeem
-        "${impure-local}"
-      ];
-      specialArgs = rec {
-        hasDesktop = true;
-        useRosetta = true;
-        intelPkgs = if (useRosetta) then nixpkgs.legacyPackages."x86_64-darwin" else null;
+      darwinConfigurations."miniskeem" = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          self.darwinModules.nixpkgsCommon
+          home-manager.darwinModules.home-manager
+          ./systems/miniskeem
+          "${impure-local}"
+        ];
+        specialArgs = rec {
+          hasDesktop = true;
+          useRosetta = true;
+          intelPkgs = if (useRosetta) then nixpkgs.legacyPackages."x86_64-darwin" else null;
+        };
       };
-    };
 
-    nixosConfigurations."meeksorkim2" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        self.nixosModules.nixpkgsCommon
-        self.nixosModules.impermanenceConfig
-        inputs.agenix.nixosModules.age
-        inputs.sops.nixosModules.sops
-        home-manager.nixosModules.home-manager
-        impermanence.nixosModules.impermanence
-        ./systems/meeksorkim2
-        "${impure-local}"
-      ];
-    };
+      nixosConfigurations."meeksorkim2" = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
+        modules = [
+          self.nixosModules.nixpkgsCommon
+          self.nixosModules.impermanenceConfig
+          inputs.agenix.nixosModules.age
+          inputs.sops.nixosModules.sops
+          home-manager.nixosModules.home-manager
+          impermanence.nixosModules.impermanence
+          ./systems/meeksorkim2
+          "${impure-local}"
+        ];
+      };
 
-    packages.x86_64-linux.kexecBootstrap = let
-      system = "x86_64-linux";
-    in inputs.nixos-generators.nixosGenerate {
-      pkgs = nixpkgs.legacyPackages.${system};
-      format = "kexec_bundle";
-      modules = [
-        ./systems/_common/nix.nix
-      ];
-    };
-  };
+      packages.x86_64-linux.kexecBootstrap =
+        inputs.nixos-generators.nixosGenerate {
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          format = "kexec-bundle";
+          modules = [
+            ./systems/_common/nix.nix
+          ];
+        };
+    } // flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell =
+          pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.sops
+              pkgs.rage
+              pkgs.ssh-to-age
+            ];
+          };
+      });
 }
