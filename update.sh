90fa8bafd403a17358b3734cac8aedf02572b0db
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-12-20 15:55:37 +0200

    Improve update script
    1) Update impure-local input
    2) Build system configuration using lower nice/priority (to not tank
       resource usage in general)
    3) Elevate permissions only when needed (during activation)
    4) Keep `result` directory (for easier debugging)
    5) Reset flake.lock (because of step 1)

--- update.sh
+++ update.sh
@@ -8,23 +8,77 @@ if ! [ -f "${flake}/flake.nix" ]; then
 	exit 1
 fi
 
+no_activate=0
+while [ -n "${1:-}" ]; do
+	arg="${1}"
+	case "${arg}" in
+		--no-activate)
+			no_activate=1
+			;;
+		*)
+			echo "unsupported option: ${arg}"
+			;;
+	esac
+	shift;
+done
+
+export NIXOS_INSTALL_BOOTLOADER=0 # TODO
+
 machine="$(uname -s)"
 hostname="$(hostname -s)"
-elevate="sudo"
+elevate=(sudo --preserve-env=NIXOS_INSTALL_BOOTLOADER)
+
+args=(--flake "${flake}#${hostname}")
+wrapper=(nice -n 5)
+
+# Ensure this is always up to date
+nix flake lock --update-input impure-local
+trap 'do_cleanup' EXIT
+
+do_activate () {
+	if [ "${no_activate}" = "1" ]; then
+		echo "skipping activation as requested"
+		return 0
+	fi
+	local res="${1}"
+	pushd "${res}" >/dev/null
+
+	profile=/nix/var/nix/profiles/system
+	pathToConfig="$(realpath -- .)"
+
+	echo "activating configuration '${pathToConfig}'"
+	"${elevate[@]}" nix-env -p "${profile}" --set "${pathToConfig}"
+
+	if [ "${machine}" = "Darwin" ] && [ -f ./activate-user ] && [ -x ./activate-user ]; then
+		# TODO: ensure that user is not root
+		./activate-user
+		"${elevate[@]}" ./activate
+	elif [ "${machine}" = "Linux" ]; then
+		if ! "${elevate[@]}" ./bin/switch-to-configuration switch; then
+			echo "new configuration activation did not succeed cleanly"
+		fi
+	fi
+
+	popd >/dev/null
+}
+
+do_cleanup () {
+	git checkout -- flake.lock
+}
 
 case "${machine}" in
 	Darwin)
-		darwin-rebuild switch --flake "${flake}#${hostname}"
-
-		# NOTE: https://github.com/LnL7/nix-darwin/issues/375
-		[ -L ./result ] && rm result
+		"${wrapper[@]}" darwin-rebuild build "${args[@]}"
+		do_activate ./result
 		;;
 	Linux)
 		if test -n "$(command -v doas &>/dev/null)"; then
-			elevate="doas"
+			elevate=(doas env NIXOS_INSTALL_BOOTLOADER="${NIXOS_INSTALL_BOOTLOADER}")
 		fi
 
-		"${elevate}" nixos-rebuild switch --flake "${flake}#${hostname}"
+		wrapper+=(chrt -i 0)
+		"${wrapper[@]}" nixos-rebuild build "${args[@]}"
+		do_activate ./result
 		;;
 	*)
 		echo "unsupported machine: ${machine}"
