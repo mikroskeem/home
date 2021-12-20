d5f759fd065e6670c176054e69a4ad79d777bfc9
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-12-20 14:25:48 +0200

    Simplify Chromium wrapper

--- pkgs/chromium-ozone-wrapper.nix
+++ pkgs/chromium-ozone-wrapper.nix
@@ -1,7 +1,14 @@
-{ stdenv, lib, chromium, writeShellScript }:
+{ stdenvNoCC, lib, chromium, runtimeShell }:
 
-let
-  script = writeShellScript "chromium-wrapper" ''
+stdenvNoCC.mkDerivation rec {
+  pname = "${lib.strings.getName chromium.name}-wayland-wrapped";
+  version = lib.strings.getVersion chromium.name;
+
+  phases = [ "installPhase" ];
+
+  wrapperScript = ''
+    #!${runtimeShell}
+    set -e
     params=()
     if [ -n "$WAYLAND_DISPLAY" ]; then
       params+=(--enable-features=UseOzonePlatform --enable-features=WebRTCPipeWireCapturer --ozone-platform=wayland --enable-usermedia-screen-capturing)
@@ -11,18 +18,14 @@ let
     fi
     exec ${chromium}/bin/chromium "''${params[@]}" $@
   '';
-in
-stdenv.mkDerivation rec {
-  pname = "${lib.strings.getName chromium.name}-wayland-wrapped";
-  version = lib.strings.getVersion chromium.name;
 
-  phases = [ "installPhase" ];
+  passAsFile = [ "wrapperScript" ];
 
   installPhase = ''
     mkdir -p $out/bin
     cd $out && ln -s ${chromium}/share
-    cd $out/bin && ln -s ${script} chromium
-    cd $out/bin && ln -s ${script} chromium-browser
+    install -D -m 755 $wrapperScriptPath $out/bin/chromium
+    ln -s chromium $out/bin/chromium-browser
   '';
 
   meta = chromium.meta // {
