c5b4ebaea27dbdce6e4c68a6c8f843e66eeb528a
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-02-27 00:00:31 +0200

    Persist mysql data directory

--- modules/impermanence.nix
+++ modules/impermanence.nix
@@ -18,6 +18,13 @@
       "/etc/rancher"
       "/var/lib/kubelet"
       "/var/lib/rancher"
+    ] ++ lib.optionals config.services.mysql.enable [
+      {
+        directory = config.services.mysql.dataDir;
+        user = "mysqld";
+        group = "mysqld";
+        mode = "u=rwx,g=,o=";
+      }
     ] ++ lib.optionals config.virtualisation.docker.enable [
       "/var/lib/docker"
     ] ++ lib.optionals (config ? declared-secrets) [
