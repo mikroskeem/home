740d79ba6a47c89a878d3b0e01fff80e8a7c7c5a
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-05-19 23:02:48 +0300

    Configure neovim

--- home/mark.nix
+++ home/mark.nix
@@ -170,9 +170,114 @@ rec {
   programs.neovim = {
     enable = true;
     plugins = with pkgs.vimPlugins; [
+      cmp-buffer
+      cmp-cmdline
+      cmp-nvim-lsp
+      cmp-path
+      cmp-vsnip
+      nvim-cmp
       nvim-lspconfig
+      rust-vim
       vim-nix
       vim-terraform
+      vim-vsnip
+      vim-which-key
+
+      #(pkgs.vimUtils.buildVimPlugin {
+      #  name = "Sierra";
+      #  src = pkgs.fetchFromGitHub {
+      #    owner = "AlessandroYorba";
+      #    repo = "Sierra";
+      #    rev = "461dc4bd1ac161b6a14f4a3194363d89abe75401";
+      #    sha256 = "sha256-k4PENXu+QDL2OPM4623QvDLCs7CtaUzBJakBYxR7zb8=";
+      #  };
+      #})
+    ];
+
+    extraConfig = builtins.concatStringsSep "\n\n" [
+      ''
+        set completeopt=menu,menuone,noselect
+        set eol
+        set termguicolors
+
+        highlight ExtraWhitespace ctermbg=red guibg=red
+        match ExtraWhitespace /\s\+\%#\@<!$/
+      ''
+      ''
+        lua <<EOF
+          -- Setup nvim-cmp.
+          local cmp = require'cmp'
+
+          cmp.setup({
+            snippet = {
+              -- REQUIRED - you must specify a snippet engine
+              expand = function(args)
+                vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+              end,
+            },
+            window = {
+              -- completion = cmp.config.window.bordered(),
+              -- documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<C-e>'] = cmp.mapping.abort(),
+              ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            sources = cmp.config.sources({
+              { name = 'nvim_lsp' },
+              { name = 'vsnip' }, -- For vsnip users.
+              -- { name = 'luasnip' }, -- For luasnip users.
+              -- { name = 'ultisnips' }, -- For ultisnips users.
+              -- { name = 'snippy' }, -- For snippy users.
+            }, {
+              { name = 'buffer' },
+            })
+          })
+
+          -- Set configuration for specific filetype.
+          -- cmp.setup.filetype('gitcommit', {
+          --   sources = cmp.config.sources({
+          --     { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+          --   }, {
+          --     { name = 'buffer' },
+          --   })
+          -- })
+
+          cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+              { name = 'buffer' }
+            }
+          })
+
+          -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+          -- cmp.setup.cmdline(':', {
+          --   mapping = cmp.mapping.preset.cmdline(),
+          --   sources = cmp.config.sources({
+          --     { name = 'path' }
+          --   }, {
+          --     { name = 'cmdline' }
+          --   })
+          -- })
+
+          -- Setup lspconfig.
+          local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+          local servers = { 'ansiblels', 'gopls', 'rnix', 'rust_analyzer', 'zls' };
+          for _, srv in ipairs(servers) do
+            require('lspconfig')[srv].setup {
+              capabilities = capabilities
+            }
+          end
+
+        EOF
+      ''
     ];
   };
 
