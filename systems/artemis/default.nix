287dd7e2276e875e5e43083818735ac42927029f
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2024-11-24 02:11:24 +0200

    Update inputs

--- systems/artemis/default.nix
+++ systems/artemis/default.nix
@@ -58,6 +58,7 @@
   };
 
   system.activationScripts.postActivation.text = ''
+    # shellcheck disable=SC2174
     mkdir -p -m 0755 /usr/local/lib/pam
     ln -svf ${pkgs.yubico-pam}/lib/security/pam_yubico.so /usr/local/lib/pam/pam_yubico.so
     ${pkgs.gnused}/bin/sed -i '2iauth sufficient pam_yubico.so mode=challenge-response # mikroskeem/home' /etc/pam.d/sudo
